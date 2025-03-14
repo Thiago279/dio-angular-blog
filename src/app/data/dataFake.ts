37f2ba784280c2ef61db7999e1aed8e4857f4d9a
Thiago Toma <thiagotoma99@gmail.com>
2025-03-14 13:28:47 -0300

    fix card-description alignment

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,25 +2,26 @@ export const dataFake = [
     {
         "id":1,
         "image":"https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/09/uniforme-3-santos-e1726260722265.jpg?w=1200&h=1200&crop=1",
-        "title":"Santos lança novo uniforme 3",
-        "description":"ola"
+        "title":"Santos FC lança novo uniforme 3",
+        "description":" O Santos FC acaba de lançar seu novo uniforme 3 para a temporada 2025, apostando em um design moderno e inovador. O novo modelo traz um visual mais arrojado, com detalhes que representam a história e as cores do clube. A camisa foi apresentada oficialmente em evento realizado na Vila Belmiro e promete ser um sucesso entre os torcedores santistas. O lançamento já está disponível nas lojas oficiais do clube e no site, com diversas opções para os fãs do Peixe."
     },
     {
         "id":2,
         "image":"https://lncimg.lance.com.br/cdn-cgi/image/width=828,quality=75,fit=pad,format=webp/uploads/2025/03/neymar-santos-corinthians-scaled-aspect-ratio-512-320.jpg",
         "title":"Neymar desfalca Santos e é dúvida para Dorival na seleção",
-        "description":"ola"
+        "description":"O Santos FC recebeu uma notícia preocupante, com Neymar Jr. sendo confirmado como desfalque para o time no próximo jogo. O jogador sofreu uma lesão leve durante um treino, o que o impossibilita de atuar pelo clube. Além disso, a presença de Neymar na seleção brasileira para os próximos amistosos está em dúvida, já que o técnico Dorival Júnior ainda aguarda a avaliação do departamento médico para decidir se o atacante fará parte da convocação. A torcida santista aguarda ansiosa pela recuperação de seu principal ícone."
+
     },
     {
         "id":3,
         "image":"https://conteudo.imguol.com.br/c/esporte/f8/2025/03/13/memphis-depay-do-corinthians-se-lamentando-durante-a-partida-contra-o-barcelona-equ-pela-libertadores-1741841277800_v2_900x506.jpg",
         "title":"Corinthians é eliminado na pre-libertadores e quebra recorde brasileiro",
-        "description":"ola"
+        "description":"O Corinthians enfrentou uma dura derrota na noite de ontem, sendo eliminado da pré-Libertadores em um confronto decisivo. Apesar da vitória por 2-O, o time paulista precisava contornar uma desvantagem de 3 gols sofridos no primeiro jogo do confronto, o que resultou na sua eliminação precoce da competição continental. Além disso, a queda na pré-Libertadores fez o Corinthians quebrar um recorde negativo: O time brasileiro com mais eliminações (3) na fase preliminar da libertadores . A torcida corinthiana está em luto, e agora o clube precisa focar nas competições nacionais para tentar se reerguer."
     },
     {
         "id":4,
         "image":"https://static.poder360.com.br/2025/02/seguidores-santos-neymar-6-fev-2025-2.png",
         "title":"Santos é brasileiro mais visto na internet no mês de Fevereiro",
-        "description":"ola"
+        "description":"O Santos FC conquistou um feito inédito e se tornou o clube brasileiro mais visto na internet durante o mês de fevereiro de 2025. Com diversas campanhas de marketing digital e grande engajamento nas redes sociais, o Peixe superou outros gigantes do futebol nacional, como Flamengo e Palmeiras, no ranking de visualizações e interações online. Esse sucesso é atribuído à crescente presença do clube no ambiente digital e aos esforços para atrair mais torcedores para as plataformas de streaming e redes sociais. O Santos continua se destacando não apenas dentro de campo, mas também no cenário digital."
     }
 ]
